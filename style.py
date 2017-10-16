33c3f28f19312592e61b25eea6a8447b82949eeb
陈修宏 <chenxh.cz@gmail.com>
2017-10-16 23:00:11 +0800

    learn object oriented programming

---
+++ style.py
@@ -0,0 +1,20 @@
+class OldSytle:
+    def __init__(self,name ,description):
+        self.name = name
+        self.desciption = description
+
+class NewSytle(object):
+    def __init__(self,name ,description):
+        self.name = name
+        self.description = description
+
+old = OldSytle('old', "old style class")
+print(old)
+print(type(old))
+print(dir(old))
+
+new = NewSytle('new', 'new style class')
+print('------------------------------------------')
+print(new)
+print(type(new))
+print(dir(new))
