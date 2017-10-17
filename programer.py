9bc78923fb6fbe45907027feaed39fb318729ff1
陈修宏 <chenxh.cz@gmail.com>
2017-10-17 22:50:57 +0800

    学完多态了

--- programer.py
+++ programer.py
@@ -1,7 +1,7 @@
 class Programer(object):
     hobby = 'Play computer'
 
-    def __init__(self,name,age,weight):
+    def __init__(self, name, age, weight):
         self.name = name
         self._age = age
         self.__weight = weight
@@ -15,12 +15,24 @@ class Programer(object):
         return self.__weight
 
     def self_introduction(self):
-        print('My name is %s \nI am %s years old\n' % (self.name,self._age))
+        print('My name is %s \nI am %s years old\n' % (self.name, self._age))
+
+
+class BackendProgramer(Programer):
+    def __init__(self, name, age, weight, language):
+        super(BackendProgramer,self).__init__(name,age,weight)
+        self.language = language
+
+    def self_introduction(self):
+        print('My name is %s \nMy favorite language is %s' % (self.name,self.language))
+
+def introduce(programer):
+    if isinstance(programer,Programer):
+        programer.self_introduction()
+
 
 if __name__ == '__main__':
-    programer = Programer("Raki",25,130)
-    print(dir(programer))
-    print(programer.__dict__)
-    print(programer.get_hobby())
-    print(programer.get_weight)
-    print(programer.self_introduction())
+    programer = Programer('Tim',23,120)
+    backend_programer = BackendProgramer("Raki", 25, 130,'Python')
+    introduce(programer)
+    introduce(backend_programer)
