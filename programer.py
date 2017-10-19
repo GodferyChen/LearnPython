07b0a2179c862d764f60df95e9766ad8c117c085
陈修宏 <chenxh.cz@gmail.com>
2017-10-19 20:36:53 +0800

    立个flag

--- programer.py
+++ programer.py
@@ -1,10 +1,49 @@
 class Programer(object):
     hobby = 'Play computer'
 
+    def __new__(cls, *args, **kwargs):
+        # print('call __new__method')
+        # print(args)
+        return super(Programer, cls).__new__(cls)
+
     def __init__(self, name, age, weight):
+        # print('call __init__method')
         self.name = name
-        self._age = age
+        if isinstance(age, int):
+            self.age = age
+        else:
+            raise Exception('age must be int')
         self.__weight = weight
+
+    def __str__(self):
+        return '%s is %s years old' % (self.name, self.age)
+
+    def __dir__(self):
+        return self.__dict__.keys()
+
+    def __getattribute__(self, item):
+        # return getattr(self,item)
+        # return self.__dict__[item]
+        return super(Programer,self).__getattribute__(item)
+
+    def __setattr__(self, key, value):
+        # setattr(self,key,value)
+        self.__dict__[key] = value
+
+    def __eq__(self, other):
+        if isinstance(other, Programer):
+            if self.age == other.age:
+                return True
+            else:
+                return False
+        else:
+            raise Exception('The type of object must be Programer')
+
+    def __add__(self, other):
+        if isinstance(other, Programer):
+            return self.age + other.age
+        else:
+            raise Exception('The type of object must be Programer')
 
     @classmethod
     def get_hobby(cls):
@@ -20,19 +59,22 @@ class Programer(object):
 
 class BackendProgramer(Programer):
     def __init__(self, name, age, weight, language):
-        super(BackendProgramer,self).__init__(name,age,weight)
+        super(BackendProgramer, self).__init__(name, age, weight)
         self.language = language
 
     def self_introduction(self):
-        print('My name is %s \nMy favorite language is %s' % (self.name,self.language))
+        print('My name is %s \nMy favorite language is %s' % (self.name, self.language))
+
 
 def introduce(programer):
-    if isinstance(programer,Programer):
+    if isinstance(programer, Programer):
         programer.self_introduction()
 
 
 if __name__ == '__main__':
-    programer = Programer('Tim',23,120)
-    backend_programer = BackendProgramer("Raki", 25, 130,'Python')
-    introduce(programer)
-    introduce(backend_programer)
+    p1 = Programer('Tim', 23, 120)
+    # p2 = Programer('Raki', 25, 130)
+    print(p1.name)
+    # backend_programer = BackendProgramer("Raki", 25, 130,'Python')
+    # introduce(programer)
+    # introduce(backend_programer)
