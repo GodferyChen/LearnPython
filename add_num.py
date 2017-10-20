13e1800bea567be4980734978fd8a12c93c8b543
陈修宏 <chenxh.cz@gmail.com>
2017-10-20 22:32:33 +0800

    立个flag

--- add_num.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image, ImageDraw
-
-def add_num(img):
-    draw = ImageDraw
---
+++ code/add_num.py
@@ -0,0 +1,5 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def add_num(img):
+    draw = ImageDraw
+    myfont = ImageFont
