b0135de9311f4bf8c2e5193219762164b808f8e7
陈修宏 <chenxh.cz@gmail.com>
2017-10-21 20:12:16 +0800

    手敲一遍

--- code/add_num.py
+++ code/add_num.py
@@ -2,4 +2,14 @@ from PIL import Image, ImageDraw, ImageFont
 
 def add_num(img):
     draw = ImageDraw
-    myfont = ImageFont
+    myfont = ImageFont.truetype('D:/windows/fonts/Arial.ttf',size=40)
+    fillcolor = '#ff0000'
+    width, height = img.size
+    draw.text((width-40,0),'99',font=myfont,fill = fillcolor)
+    img.save('result.jpg','jpeg')
+
+    return 0
+
+if __name__ == '__main__':
+    image = Image.open('image.jpg')
+    add_num(image)
