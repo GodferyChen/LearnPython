1e91e7188c0c774669d0fd1614b176d0cc8ace21
陈修宏 <chenxh.cz@gmail.com>
2017-11-08 23:06:03 +0800

    学习函数式编程

---
+++ code/functional/sample.py
@@ -0,0 +1,16 @@
+import math
+
+
+def add(x, y, f):
+    return f(x) + f(y)
+
+
+print(add(-5, 9, abs))
+
+print(add(25, 9, math.sqrt))
+
+def format_name(s):
+    return s.capitalize()
+
+
+print(list(map(format_name, ['adam', 'LISA', 'barT'])))
