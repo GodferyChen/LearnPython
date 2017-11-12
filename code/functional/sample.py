024604073ff5211187cbe76f69e34d4c7aa0edb4
陈修宏 <chenxh.cz@gmail.com>
2017-11-12 19:15:16 +0800

    今天不想编程

--- code/functional/sample.py
+++ code/functional/sample.py
@@ -3,31 +3,31 @@ from functools import reduce
 import functools
 
 
-# def add(x, y, f):
-#     return f(x) + f(y)
-#
-#
-# print(add(-5, 9, abs))
-#
-# print(add(25, 9, math.sqrt))
-#
-#
-# def format_name(s):
-#     return s.capitalize()
-#
-#
-# print(list(map(format_name, ['adam', 'LISA', 'barT'])))
-#
-# def prod(x, y):
-#     return x*y
-#
-# print(reduce(prod, [2, 4, 5, 7, 12]))
-#
-# def is_sqr(x):
-#     r = int(math.sqrt(x))
-#     return r*r==x
-#
-# print(list(filter(is_sqr, range(1, 101))))
+def add(x, y, f):
+    return f(x) + f(y)
+
+
+print(add(-5, 9, abs))
+
+print(add(25, 9, math.sqrt))
+
+
+def format_name(s):
+    return s.capitalize()
+
+
+print(list(map(format_name, ['adam', 'LISA', 'barT'])))
+
+def prod(x, y):
+    return x*y
+
+print(reduce(prod, [2, 4, 5, 7, 12]))
+
+def is_sqr(x):
+    r = int(math.sqrt(x))
+    return r*r==x
+
+print(list(filter(is_sqr, range(1, 101))))
 
 
 def cmp_ignore_case(s1, s2):
