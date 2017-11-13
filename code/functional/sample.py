43f7427f92e8e2f80f006aa2e4be6b45c336fb89
陈修宏 <chenxh.cz@gmail.com>
2017-11-13 19:42:24 +0800

    学习函数式编程5

--- code/functional/sample.py
+++ code/functional/sample.py
@@ -18,14 +18,18 @@ def format_name(s):
 
 print(list(map(format_name, ['adam', 'LISA', 'barT'])))
 
+
 def prod(x, y):
-    return x*y
+    return x * y
+
 
 print(reduce(prod, [2, 4, 5, 7, 12]))
 
+
 def is_sqr(x):
     r = int(math.sqrt(x))
-    return r*r==x
+    return r * r == x
+
 
 print(list(filter(is_sqr, range(1, 101))))
 
@@ -44,4 +48,31 @@ def cmp_ignore_case(s1, s2):
     if x == y:
         return 0
 
+
 print(list(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower)))
+
+
+# python中返回函数
+def calc_prod(lst):
+    def lazy_prod():
+        def f(x, y):
+            return x * y
+        return reduce(f, lst, 1)
+    return lazy_prod
+f = calc_prod([1, 2, 3, 4])
+print(f())
+
+# python中闭包
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f(j):
+            def g():
+                return j*j
+            return g
+        r = f(i)
+        fs.append(r)
+    return fs
+
+f1, f2, f3 = count()
+print(f1(), f2(), f3())
