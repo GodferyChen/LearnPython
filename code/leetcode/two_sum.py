c649b5a2e50d5d8cf335c117a871d4d819a2ac99
陈修宏 <chenxh.cz@gmail.com>
2017-11-29 21:55:04 +0800

    two sum python solution

---
+++ code/leetcode/two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    @staticmethod
+    def twosum(nums, target):
+        dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dict:
+                return [dict[target - nums[i]], i]
+            if nums[i] not in dict:
+                dict[nums[i]] = i
